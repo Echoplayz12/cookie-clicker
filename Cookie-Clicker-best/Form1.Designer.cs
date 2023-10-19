a43967492afc16a5c8a6e921f33ae5d236710aeb
Echoplayz12 <143616844+Echoplayz12@users.noreply.github.com>
2023-10-19 13:38:03 -0400

    added a upgrade and got the cookie working (get rid of txt)

--- Cookie-Clicker-best/Form1.Designer.cs
+++ Cookie-Clicker-best/Form1.Designer.cs
@@ -31,7 +31,6 @@ namespace Cookie_Clicker_best
         {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.cookie_click_btn = new System.Windows.Forms.Button();
             this.lbl_points = new System.Windows.Forms.Label();
             this.txt_moneyDisplay = new System.Windows.Forms.TextBox();
             this.UpdateCookies = new System.Windows.Forms.Timer(this.components);
@@ -43,24 +42,14 @@ namespace Cookie_Clicker_best
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.PassiveCookies = new System.Windows.Forms.Timer(this.components);
             this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.tac_upgrades.SuspendLayout();
             this.tabPage1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
-            // 
-            // cookie_click_btn
-            // 
-            this.cookie_click_btn.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.cookie_click_btn.Location = new System.Drawing.Point(2, 1);
-            this.cookie_click_btn.Name = "cookie_click_btn";
-            this.cookie_click_btn.Size = new System.Drawing.Size(358, 488);
-            this.cookie_click_btn.TabIndex = 1;
-            this.cookie_click_btn.Text = "button1";
-            this.cookie_click_btn.UseVisualStyleBackColor = true;
-            this.cookie_click_btn.Visible = false;
-            this.cookie_click_btn.Click += new System.EventHandler(this.cookie_click_btn_Click);
             // 
             // lbl_points
             // 
@@ -86,7 +75,7 @@ namespace Cookie_Clicker_best
             // UpdateCookies
             // 
             this.UpdateCookies.Enabled = true;
-            this.UpdateCookies.Interval = 10;
+            this.UpdateCookies.Interval = 1;
             this.UpdateCookies.Tick += new System.EventHandler(this.UpdateCookies_Tick);
             // 
             // tac_upgrades
@@ -102,6 +91,8 @@ namespace Cookie_Clicker_best
             // tabPage1
             // 
             this.tabPage1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
             this.tabPage1.Controls.Add(this.pictureBox3);
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
@@ -114,23 +105,24 @@ namespace Cookie_Clicker_best
             // 
             this.pictureBox3.Cursor = System.Windows.Forms.Cursors.Hand;
             this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
-            this.pictureBox3.Location = new System.Drawing.Point(6, 6);
+            this.pictureBox3.Location = new System.Drawing.Point(6, 15);
             this.pictureBox3.Name = "pictureBox3";
             this.pictureBox3.Size = new System.Drawing.Size(42, 39);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox3.TabIndex = 0;
             this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
             this.pictureBox3.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox3_MouseMove);
             // 
             // tabPage2
             // 
+            this.tabPage2.BackColor = System.Drawing.Color.DeepSkyBlue;
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage2.Size = new System.Drawing.Size(257, 397);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "power up\'s";
-            this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // pictureBox1
             // 
@@ -141,6 +133,7 @@ namespace Cookie_Clicker_best
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // pictureBox2
             // 
@@ -158,21 +151,39 @@ namespace Cookie_Clicker_best
             this.PassiveCookies.Interval = 1000;
             this.PassiveCookies.Tick += new System.EventHandler(this.PassiveCookies_Tick);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 3);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1021, 583);
+            this.ClientSize = new System.Drawing.Size(1021, 485);
             this.Controls.Add(this.tac_upgrades);
             this.Controls.Add(this.txt_moneyDisplay);
             this.Controls.Add(this.lbl_points);
-            this.Controls.Add(this.cookie_click_btn);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.pictureBox2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.tac_upgrades.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
@@ -182,10 +193,8 @@ namespace Cookie_Clicker_best
         }
 
         #endregion
-        private System.Windows.Forms.Button cookie_click_btn;
         private System.Windows.Forms.Label lbl_points;
         private System.Windows.Forms.TextBox txt_moneyDisplay;
-        private System.Windows.Forms.Timer UpdateCookies;
         private System.Windows.Forms.TabControl tac_upgrades;
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.TabPage tabPage2;
@@ -194,6 +203,9 @@ namespace Cookie_Clicker_best
         private System.Windows.Forms.Timer PassiveCookies;
         private System.Windows.Forms.PictureBox pictureBox3;
         private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Timer UpdateCookies;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Cookie-Clicker-best/Form1.cs
+++ Cookie-Clicker-best/Form1.cs
@@ -15,6 +15,7 @@ namespace Cookie_Clicker_best
         int cookiePress = 1;
         int total = 0;
         int passiveIncome = 0;
+
         public Form1()
         {
           
@@ -24,25 +25,52 @@ namespace Cookie_Clicker_best
 
         private void cookie_click_btn_Click(object sender, EventArgs e)
         {
-            // The Btn is not visible but the picture is so when the game starts they only see the cookie.
-            total += cookiePress;
-
+            
         }
 
-
+        //updates the text box with the total amount of cookies you have
         private void UpdateCookies_Tick(object sender, EventArgs e)
         {
             txt_moneyDisplay.Text = total.ToString();
         }
 
+        //this adds the passive cookies with the total amount of cookies you have 
         private void PassiveCookies_Tick(object sender, EventArgs e)
         {
-            txt_moneyDisplay.Text = PassiveCookies.ToString();
+            total += passiveIncome;
         }
-
+        
         private void pictureBox3_MouseMove(object sender, MouseEventArgs e)
         {
-            toolTip1.SetToolTip(pictureBox3, "curser will click for you buy +1"); // you can change the first parameter (textbox3) on any control you wanna focus
+            toolTip1.SetToolTip(pictureBox3, "curser will click for you buy"); // you can change the first parameter (textbox3) on any control you wanna focus
         }
-    }
+
+        //this is the first upgrade you get it adds +1 to your passive cookies 
+        private void Btn_hand_Click(object sender, EventArgs e)
+        {
+        
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            // The Btn is not visible but the picture is so when the game starts they only see the cookie.
+            total += cookiePress;
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            // if you dont have the money then you get a error message but if you do then the money you have will be subtracted by the cost then +1 will be added 
+            int handUpgrade = 1;
+            if (total > 100)
+            {
+                passiveIncome += handUpgrade;
+                int handUpgradesub = 100;
+                total -= handUpgradesub;
+            }
+            else
+            {
+                MessageBox.Show("You dont have the sufficant funds", "error", MessageBoxButtons.OK);
+            }
+        }
+    }   
 }
