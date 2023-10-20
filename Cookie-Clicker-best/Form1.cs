87a55705df90f9d68c7270e269acd1918ffd08a5
Echoplayz12 <143616844+Echoplayz12@users.noreply.github.com>
2023-10-20 06:28:11 -0400

    xcxkljcjkxdckjd

--- Cookie-Clicker-best/Form1.cs
+++ Cookie-Clicker-best/Form1.cs
@@ -12,26 +12,24 @@ namespace Cookie_Clicker_best
 {
     public partial class Form1 : Form
     {
-        int cookiePress = 1;
-        int total = 0;
-        int passiveIncome = 0;
+        #region
+        Double cookiePress = 1;
+        Double total = 0;
+        Double passiveIncome = 0;
+        Double Upgradesub = 100;
+        int handUpgrade = 1;
 
+        #endregion
         public Form1()
         {
-          
             InitializeComponent();
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-        }
-
-        private void cookie_click_btn_Click(object sender, EventArgs e)
-        {
-            
+            label1.Text = "cost:" + 100.ToString();
         }
 
         //updates the text box with the total amount of cookies you have
         private void UpdateCookies_Tick(object sender, EventArgs e)
         {
-            txt_moneyDisplay.Text = total.ToString();
+            txt_moneyDisplay.Text = Math.Round(total).ToString();
         }
 
         //this adds the passive cookies with the total amount of cookies you have 
@@ -39,16 +37,11 @@ namespace Cookie_Clicker_best
         {
             total += passiveIncome;
         }
-        
+
         private void pictureBox3_MouseMove(object sender, MouseEventArgs e)
         {
-            toolTip1.SetToolTip(pictureBox3, "curser will click for you buy"); // you can change the first parameter (textbox3) on any control you wanna focus
-        }
-
-        //this is the first upgrade you get it adds +1 to your passive cookies 
-        private void Btn_hand_Click(object sender, EventArgs e)
-        {
-        
+            // you can change the first parameter (textbox3) on any control you wanna focus
+            toolTip1.SetToolTip(pictureBox3, "curser will click for you buy");
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
@@ -60,17 +53,28 @@ namespace Cookie_Clicker_best
         private void pictureBox3_Click(object sender, EventArgs e)
         {
             // if you dont have the money then you get a error message but if you do then the money you have will be subtracted by the cost then +1 will be added 
-            int handUpgrade = 1;
-            if (total > 100)
+            // sub is the the subtracting varible
+
+            if (Buy(Upgradesub, 1))
             {
-                passiveIncome += handUpgrade;
-                int handUpgradesub = 100;
-                total -= handUpgradesub;
+                Upgradesub *= 1.05;
+                label1.Text = "cost:" + ((int)Upgradesub).ToString();
+            }
+        }
+
+        private bool Buy(Double cost, double passiveIncomeIncrease)
+        {
+            if (cost <= total)
+            {
+                total -= cost;
+                passiveIncome += passiveIncomeIncrease;
+                return true;
             }
             else
             {
                 MessageBox.Show("You dont have the sufficant funds", "error", MessageBoxButtons.OK);
+                return false;
             }
         }
-    }   
+    }
 }
